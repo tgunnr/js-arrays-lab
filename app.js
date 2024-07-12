41dc765172f11d8fdc86386169f95b5c602159bc
tgunnr <finalresidentevilfantasy@gmail.com>
2024-07-11 20:28:09 -0700

    Exercise 13

--- app.js
+++ app.js
@@ -194,3 +194,27 @@ console.log('Exercise 12 Results:');
 console.log('  fizz:', fizz);
 console.log('  buzz:', buzz);
 console.log('  fizzbuzz:', fizzbuzz);
+
+
+/*
+Exercise 13: Retrieve the Last Array
+
+1) Assign the last nested array in the `numArrays` below to a variable named
+   `numList`. As you do this, also fulfill these goals:
+
+   - Assume you don't know how many nested arrays `numArrays` contains.
+   - Do not alter the original `numArrays` array.
+
+Complete Exercise 13 in the space below:
+*/
+
+const numArrays = [
+	[100, 5, 23],
+	[15, 21, 72, 9],
+	[45, 66],
+	[7, 81, 90]
+];
+
+const numList = numArrays[numArrays.length - 1][(numArrays[numArrays.length - 1]).length - 1]
+
+console.log('Exercise 13 result:', numList);
